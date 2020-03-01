4b1174044f9af24e30fd1ebe3511e24dcd18e303
santiagodls <itnas06@gmail.com>
2020-03-01 21:01:41 +0100

    Layout width

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,32 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <nuxt />
 </template>
 
 <style lang="stylus">
 html
   font-family: 'Source Sans Pro';
-  font-size: 16px;
+  font-size: 18px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+
+html,
+body,
+#__nuxt,
+#__layout
+  width: 100%;
+  height: 100%;
+
+#__nuxt
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+#__layout
+  max-width: 80em;
 
 *,
 *:before,
