839e9ee67a463260a2766d4eb97c0af539dba3fc
santiagodls <itnas06@gmail.com>
2020-03-22 14:22:31 +0100

    Load element-ui elements

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -2,6 +2,16 @@
   nuxt
 </template>
 
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import 'element-ui/lib/theme-chalk/index.css'
+
+@Component
+export default class BlogLayout extends Vue {
+
+}
+</script>
+
 <style lang="stylus">
 @import '~@/assets/theme';
 @import '~@/assets/mixins';
