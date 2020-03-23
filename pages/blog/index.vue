627383cc667f7bc7c3763feeb353bfe62d38f580
santiagodls <itnas06@gmail.com>
2020-03-23 23:18:32 +0100

    Styles for entries list

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,10 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import { parseISO, format } from 'date-fns'
+import { es } from 'date-fns/locale'
 import Api, { ApiResponse } from '@/helpers/api'
+
+type Entry = ApiResponse['results'][number]
 
 interface ListPagination {
   previousPage: boolean
@@ -30,6 +34,22 @@ export default class Blog extends Vue {
     this.data = await Api.getBlogEntries({ page })
   }
 
+  private _getEntryDay (entry: Entry): string {
+    if (!entry || !entry.first_publication_date) return ''
+    const date = parseISO(entry.first_publication_date)
+    return format(date, 'dd')
+  }
+
+  private _getEntryMonth (entry: Entry): string {
+    if (!entry || !entry.first_publication_date) return ''
+    const date = parseISO(entry.first_publication_date)
+    return format(date, 'MMMM', { locale: es })
+  }
+
+  private _getEntryText (entry: Entry): string {
+    return entry.data.content.map((datum: any) => datum.text).join('\n')
+  }
+
   private _onPreviousButtonClicked () {
     this.fetchEntries(this.pagination.currentPage - 1)
   }
@@ -47,12 +67,18 @@ main
     h2.subtitle Un pequeño espacio para las ideas que hay que compartir, para las historias que creemos que hay que contar
   section.list-section
     nav.list-entries(v-if='data')
-      nuxt-link.list-entry(
+      .list-entry(
         v-for='entry, index of data.results'
-        :to='`/blog/${entry.uid}`'
         :key='index'
       )
-        h3.list-entry-title {{ entry.data.title[0].text }}
+        .list-entry-left
+          span.list-entry-day {{ _getEntryDay(entry) }}
+          span.list-entry-month {{ _getEntryMonth(entry) }}
+        .list-entry-right
+          nuxt-link.list-entry-title(:to='`/blog/${entry.uid}`')
+            | {{ entry.data.title[0].text }}
+          p.list-entry-content
+            | {{ _getEntryText(entry) }}
     footer.list-footer
       .left-section
         button.footer-button(
@@ -114,9 +140,37 @@ main
     list-style: none;
 
   &-entry
-    text-decoration: none;
+    display: flex;
+    &:not(:last-child)
+      margin-bottom: 6em;
+
+  &-entry-left
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-weight: bold;
+    color: $app-grey;
+    flex-shrink: 0;
+    margin-right: 2em;
+
+  &-entry-day
+    font-size: 8em;
+    line-height: .75;
+
+  &-entry-month
+    font-size: 2.5em;
 
   &-entry-title
-    font-size: 1.5em;
+    text-decoration: none;
+    color: $dark-text-primary;
+    font-size: 2em;
+    margin-top: 1em;
+
+  &-entry-content
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 5;
+    max-height: 6.5em;
 
 </style>
