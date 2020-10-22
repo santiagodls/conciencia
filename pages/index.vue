baa9e1584048a947c37057b38b2f35a31168c162
santiagodls <itnas06@gmail.com>
2020-10-22 07:24:24 +0200

    fix(index): remove hiring modal

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import TheContactForm from '@/components/TheContactForm.vue'
-import TheHiringModal from '@/components/TheHiringModal.vue'
 
-@Component({ components: { TheContactForm, TheHiringModal } })
+@Component({ components: { TheContactForm } })
 export default class Index extends Vue {}
 
 </script>
@@ -55,8 +54,6 @@ main
     picture
       source(media="(max-width: 780px)" srcset="@/static/img7-mobile.png")
       img.section-image(src="@/static/img7.png")
-
-  TheHiringModal
 
 </template>
 
